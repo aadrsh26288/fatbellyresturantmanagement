59880490e1d00e21de02bfbc1f69be9f272ff206
rubansundararaj <rubansundararaj@gmail.com>
2022-01-30 01:28:31 +0800

    show menu and categories

--- src/data/MenuList.js
+++ src/data/MenuList.js
@@ -3,37 +3,37 @@ export const menuList = {
         {
         name : "Truffle Mushroom Soba",
         category : "Soba",
-        image : "grub2sleexht.png",
+        image : "./../images/grub2sleexht.png",
         price : "15"
         },
         {
         name : "Double Cheeseburger",
         category : "Burger",
-        image : "grub2sleexht.png",
+        image : "./../images/grub2sleexht.png",
         price : "20"
         },
         {
         name : "Mala Xiang Guo Burger",
         category : "Burger",
-        image : "grub2sleexht.png",
+        image : "./../images/grub2sleexht.png",
         price : "25"
         },
         {
         name : "Maple Bacon Burger",
         category : "Burger",
-        image : "grub2sleexht.png",
+        image : "./../images/grub2sleexht.png",
         price : "15"
         },
         {
         name : "Grilled Chicken Pesto",
-        category : "grub2sleexht.png",
-        image : "",
+        category : "Grilled",
+        image : "./../images/grub2sleexht.png",
         price : "18"
         },
         {
         name : "Grilled Ribeye Steak",
         category : "Grilled",
-        image : "grub2sleexht.png",
+        image : "./../images/grub2sleexht.png",
         price : "25"
         }
 ]
--- src/page/Menu.js
+++ src/page/Menu.js
@@ -1,9 +1,15 @@
-import { Flex,
+import { Box, Button, Flex,
     Image,} from '@chakra-ui/react';
 import React from 'react';
 
 import { resturantInfo } from '../data/ResturantInfo';
+import { categories } from '../data/Categories';
+import { menuList } from '../data/MenuList';
+
 import Navbar from '../components/Navbar';
+import IndividualItems from '../components/IndividualItems';
+import CategoriesItems from '../components/CategoriesItems';
+
 
 
 export default function Menu() {
@@ -13,6 +19,23 @@ export default function Menu() {
          <Flex justifyContent="center" alignContent="center" my={2}>
             <Image minW={"300px"} maxW={"300px"}  src={resturantInfo.logo}/>
             </Flex>
+            <CategoriesItems styleAColorHex={'#FF2C55'}  uniqueCategory={categories.categories}/>
+           <Flex flexWrap='wrap' m='5'  justifyContent='center'  borderColor='gray.100' alignContent={"center"}>
+             
+           {menuList.items.map((item,index)=>(
+                    <Box key={index}>
+                        <IndividualItems currency={"SGD"}  item={item} styleAColorHex={'#FF2C55'} />
+                    </Box>           
+                ))}  
+
+
+         </Flex>
+         <Flex flexWrap='wrap' m='5'  alignContent={"center"}>
+        <Button bg={'grey'} color='white' onClick={()=>{}}>
+                 
+                    Back
+                </Button> 
+                </Flex>
     </>
   );
 }
